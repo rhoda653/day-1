2f5ec5b8525f83db79d5458973c63395d77398a5
Rhoda <rhodamingotamanjong@gmail.com>
2025-03-22 17:51:07 +0100

    day

---
+++ assets/pages/home.js
@@ -0,0 +1,14 @@
+// alert("connected......")
+const getstarted = document.getElementById("getstartedbtn")
+const popup = document.getElementById("popupcontainer")
+const close = document.getElementById("close-btn")
+console.log(getstarted)
+
+// events are what happens when i carry out a particular action
+
+getstarted.addEventListener('click', function(){
+    popup.style.display = "flex"
+})
+
+// ASSIGNMENT: RESEARCH ON;
+// 
