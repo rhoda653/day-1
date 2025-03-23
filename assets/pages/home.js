dec06d5bab9603fb1bc2c75e45fe45a544cf7588
Rhoda <rhodamingotamanjong@gmail.com>
2025-03-23 14:58:50 +0100

    day3

--- assets/pages/home.js
+++ assets/pages/home.js
@@ -2,6 +2,7 @@
 const getstarted = document.getElementById("getstartedbtn")
 const popup = document.getElementById("popupcontainer")
 const close = document.getElementById("close-btn")
+const form = document.getElementById("formContainer")
 console.log(getstarted)
 
 // events are what happens when i carry out a particular action
@@ -9,6 +10,22 @@ console.log(getstarted)
 getstarted.addEventListener('click', function(){
     popup.style.display = "flex"
 })
+close.addEventListener('click', function(){
+    popup.style.display = "none"
+    // console.log("you clicked me.....")
+})
+form.addEventListener('submit', function(){
+    // perform action when we submit the form
+    // we need the password value and the confirm password value
+    const password = document.getElementById('password').value
+    const confirmpassword = document.getElementById('confirmpassword').value
+    // console.log("this is my password", password)
+    if (password!==confirmpassword){
+        alert("password does not match")
+        return;
+    }
+})
+
 
 // ASSIGNMENT: RESEARCH ON;
 // 
